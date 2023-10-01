70cdde5eeb20f4444c582fd36481734d49bd646b
JamesBond73 <fpgaspar73@gmail.com>
2023-10-01 13:03:40 +0100

    Added Killer Moves and History Heuristics / Improved Move Scorer/Sorter

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,7 +7,8 @@ public class MyBot : IChessBot
     int[] pieceVal = { 0, 100, 310, 330, 500, 1000, 10000 };
     int[] piecePhase = { 0, 0, 1, 1, 2, 4, 0 };
     ulong[] psts = { 657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569, 366006824779723922, 366006826859316500, 786039115310605588, 421220596516513823, 366011295806342421, 366006826859316436, 366006896669578452, 162218943720801556, 440575073001255824, 657087419459913430, 402634039558223453, 347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514, 329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 257053881053295759, 291134268204721362, 492947507967247313, 367159395376767958, 384021229732455700, 384307098409076181, 402035762391246293, 328847661003244824, 365712019230110867, 366002427738801364, 384307168185238804, 347996828560606484, 329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047, 347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 384289231362147538, 384307167128540502, 366006826859320596, 366006826876093716, 366002360093332756, 366006824694793492, 347992428333053139, 457508666683233428, 329723156783776785, 329401687190893908, 366002356855326100, 366288301819245844, 329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863, 419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 366855645423297932, 329991151972070674, 311105941360174354, 256772197720318995, 365993560693875923, 258219435335676691, 383730812414424149, 384601907111998612, 401758895947998613, 420612834953622999, 402607438610388375, 329978099633296596, 67159620133902 };
-    //private readonly Move[] killers = new Move[2048];
+    private readonly Move[] killers = new Move[2048];
+    private readonly int[] moveScores = new int[218];
 
     struct Transposition
     {
@@ -41,6 +42,9 @@ public class MyBot : IChessBot
         futilityMovePrunned = 0; // #DEBUG
 
         Move bestmoveRoot = Move.NullMove;
+
+        // Reset history tables
+        var historyHeuristics = new int[2, 7, 64];
 
         bool do_null = true;
 
@@ -119,6 +123,7 @@ public class MyBot : IChessBot
             bool qsearch = intDepth <= 0;
             bool notRoot = ply > 0;
             int best = -99_999_999;
+            int movesScored = 0;
 
             // Check for repetition
             if (notRoot && board.IsRepeatedPosition())
@@ -173,36 +178,38 @@ public class MyBot : IChessBot
 
             }
             do_null = true;
+
+
             // Move Gen
-            Move[] moves = board.GetLegalMoves(qsearch);
-            int[] moveScores = new int[moves.Length];
+            Span<Move> moveSpan = stackalloc Move[218];
+            board.GetLegalMovesNonAlloc(ref moveSpan, qsearch && !board.IsInCheck());
 
-            // Score Moves
-            for (int i = 0; i < moves.Length; i++)
-            {
-                //TT
-                if (moves[i] == transposition.move) moveScores[i] -= 1_000_000;
+            foreach (Move move in moveSpan)
+                moveScores[movesScored++] = -(
+                // Hash move
+                move == transposition.move ? 9_000_000 :
+                
+                // MVVLVA
+                move.IsCapture ? 1_000_000 * (int)move.CapturePieceType - (int)move.MovePieceType :
+                
+                // Killers
+                killers[ply] == move ? 900_000 :
+                
+                // History
+                historyHeuristics[ply & 1, (int)move.MovePieceType, move.TargetSquare.Index]);
 
-                //MVVLVA
-                if (moves[i].IsCapture) moveScores[i] -= 100 * (int)moves[i].CapturePieceType - (int)moves[i].MovePieceType;
-
-                //Killer Moves
-                //if (moves[i] == killers[ply]) moveScores[i] -= 900_000;
-
-            }
-
-            // Sort Moves based on their score
-            Array.Sort(moveScores, moves);
+            moveScores.AsSpan(0, moveSpan.Length)
+                .Sort(moveSpan);
 
             Move bestMove = Move.NullMove;
             int startingAlpha = alpha;
 
             // Search moves
-            for (int i = 0; i < moves.Length; i++)
+            for (int i = 0; i < moveSpan.Length; i++)
             {
                 if (timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) return 98888;
 
-                Move move = moves[i];
+                Move move = moveSpan[i];
                 board.MakeMove(move);
                 int score = -Negamax(-beta, -alpha, intDepth - 1, ply + 1);
                 board.UndoMove(move);
@@ -219,10 +226,11 @@ public class MyBot : IChessBot
                     if (alpha >= beta)
                     {
                         movesPruned++; // #DEBUG
-                        /*if (!move.IsCapture)
+                        if (!move.IsCapture)
                         {
+                            historyHeuristics[ply & 1, (int)move.MovePieceType, move.TargetSquare.Index] += intDepth * intDepth;
                             killers[ply] = move;
-                        }*/
+                        }
                         break;
                     }
                         
@@ -231,7 +239,7 @@ public class MyBot : IChessBot
             }
 
             // (Check/Stale)mate
-            if (!qsearch && moves.Length == 0) return board.IsInCheck() ? -99999 + ply : 0;
+            if (!qsearch && moveSpan.Length == 0) return board.IsInCheck() ? -99999 + ply : 0;
 
             transposition.evaluation = best;
             transposition.zobristHash = key;
